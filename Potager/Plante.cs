8cf0a4889f95fc9de6e629e4d8dda93f7c5ffc11
SchneiderArthur <aschneide003@ensc.fr>
2025-04-25 11:22:03 +0200

    creation plante et maladie

---
+++ Potager/Plante.cs
@@ -0,0 +1,32 @@
+public abstract class Plante
+{
+    public string Nature { get; set; }
+    public List<string> SaisonsSemis { get; set; }
+    public string TerrainPrefere { get; set; }
+    public float Espacement { get; set; }
+    public float EspaceNecessaire { get; set; }
+    public float VitesseDeCroissance { get; set; }
+    public float BesoinsEau { get; set; }
+    public float BesoinsLuminosite { get; set; }
+    public float TemperatureMin { get; set; }
+    public float TemperatureMax { get; set; }
+    public List<Maladie> Maladies { get; set; }
+    public int EsperanceVie { get; set; }
+    public int Production { get; set; }
+
+    public Plante()
+    {
+        SaisonsSemis = new List<string>();
+        Maladies = new List<Maladie>();
+    }
+
+    public virtual void GererCroissance()
+    {
+        Console.WriteLine("La plante " + Nature + " croît.");
+    }
+
+    public virtual void Produire()
+    {
+        Console.WriteLine("La plante produit " + Production + " produit(s).");
+    }
+}
